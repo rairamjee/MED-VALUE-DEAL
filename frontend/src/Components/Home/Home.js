4a97e3a76fcb25aa5755d35b19202205f0856573
rairamjee <110187607+rairamjee@users.noreply.github.com>
2024-05-20 03:27:33 +0530

    Home Page Added
    Carousel still to be added and some changes need to be made accordingly

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Home.css';
-
-const Home = () => {
+import './responsive.css';
+import InfoSection from './InfoSection';
+const HomePage = () => {
   return (
     <div>
       <div id="looking">
@@ -9,18 +10,30 @@ const Home = () => {
           <h3>What are you looking for?</h3>
         </div>
         <div className="looking-img">
-          {[1, 2, 3].map((item) => (
-            <div key={item}>
-              <a href="./productpage.html">
-                <img src={`./images/looking/${item}.jpg`} alt="" />
-              </a>
-            </div>
-          ))}
-          {[4, 5, 6, 7, 8].map((item) => (
-            <div key={item}>
-              <img src={`./images/looking/${item}.jpg`} alt="" />
-            </div>
-          ))}
+          <div>
+            <a href="./productpage.html"><img src="https://medvalue5.web.app/images/looking/1.jpg" alt="" /></a>
+          </div>
+          <div>
+            <a href="./productpage.html"><img src="https://medvalue5.web.app/images/looking/2.jpg" alt="" /></a>
+          </div>
+          <div>
+            <a href="./productpage.html"><img src="https://medvalue5.web.app/images/looking/3.jpg" alt="" /></a>
+          </div>
+          <div>
+            <a href="./productpage.html"><img src="https://medvalue5.web.app/images/looking/4.jpg" alt="" /></a>
+          </div>
+          <div>
+            <a href="./productpage.html"><img src="https://medvalue5.web.app/images/looking/5.jpg" alt="" /></a>
+          </div>
+          <div>
+            <a href="./productpage.html"><img src="https://medvalue5.web.app/images/looking/6.jpg" alt="" /></a>
+          </div>
+          <div>
+            <a href="./productpage.html"><img src="https://medvalue5.web.app/images/looking/7.jpg" alt="" /></a>
+          </div>
+          <div>
+            <a href="./productpage.html"><img src="https://medvalue5.web.app/images/looking/8.jpg" alt="" /></a>
+          </div>
         </div>
       </div>
 
@@ -28,174 +41,191 @@ const Home = () => {
         <img src="https://cdn01.pharmeasy.in/dam/banner/banner/836fef100bc-YAYY550.jpg" alt="" />
         <img src="https://cdn01.pharmeasy.in/dam/banner/banner/760565c68b5-VIBES27.jpg" alt="" />
       </div>
-      <div className="offerH">
-        <h2>Offers Just for You</h2>
-      </div>
+      <div className="offerH"><h1><b>Offers Just for You</b></h1></div>
       <div id="offer2">
-        {[1, 2].map((item) => (
-          <a href="" key={item}>
-            <img src={`./images/offer/${item}.jpg`} alt="" />
-          </a>
-        ))}
-        <a href="./signup.html">
-          <img src="./images/offer/3.jpg" alt="" />
-        </a>
+        <a href=""><img src="https://medvalue5.web.app/images/offer/1.jpg" alt="" /></a>
+        <a href=""><img src="https://medvalue5.web.app/images/offer/2.jpg" alt="" /></a>
+        <a href="./signup.html"><img src="https://medvalue5.web.app/images/offer/3.jpg" alt="" /></a>
       </div>
-      <div className="offerH2">
-        <h2>Payment Offers</h2>
-      </div>
+      <div className="offerH2"><h1><b>Payment Offers</b></h1></div>
       <div id="offer3">
-        {[
-          'https://cdn01.pharmeasy.in/dam/banner/banner/a93d45ef9eb-HSBC.jpg?dim=1440x0',
-          'https://cdn01.pharmeasy.in/dam/banner/banner/f3c446e4108-AMAZON.jpg?dim=1440x0',
-          'https://cdn01.pharmeasy.in/dam/banner/banner/9466405c308-au.jpg?dim=1440x0',
-        ].map((src, index) => (
-          <img src={src} alt="" key={index} />
-        ))}
+        <img src="https://cdn01.pharmeasy.in/dam/banner/banner/a93d45ef9eb-HSBC.jpg?dim=1440x0" alt="" />
+        <img src="https://cdn01.pharmeasy.in/dam/banner/banner/f3c446e4108-AMAZON.jpg?dim=1440x0" alt="" />
+        <img src="https://cdn01.pharmeasy.in/dam/banner/banner/9466405c308-au.jpg?dim=1440x0" alt="" />
       </div>
 
       <div className="top">
         <div className="container">
           <div className="slide-container">
-            {[1, 2, 3, 4, 5, 7, 6, 8].map((item) => (
-              <div key={item}>
-                <div className="slide-img">
-                  <img src={`./images/slider1/s${item}.jpg`} alt="" />
-                </div>
-              </div>
-            ))}
+            <div className="slide-img">
+              <img src="./images/slider1/s1.jpg" alt="" />
+            </div>
+            <div className="slide-img">
+              <img src="./images/slider1/s2.jpg" alt="" />
+            </div>
+            <div className="slide-img">
+              <img src="./images/slider1/s3.jpg" alt="" />
+            </div>
+            <div className="slide-img">
+              <img src="./images/slider1/s4.jpg" alt="" />
+            </div>
+            <div className="slide-img">
+              <img src="./images/slider1/s5.jpg" alt="" />
+            </div>
+            <div className="slide-img">
+              <img src="./images/slider1/s7.jpg" alt="" />
+            </div>
+            <div className="slide-img">
+              <img src="./images/slider1/s6.jpg" alt="" />
+            </div>
+            <div className="slide-img">
+              <img src="./images/slider1/s8.jpg" alt="" />
+            </div>
           </div>
         </div>
       </div>
 
       <div id="cat">
-        <div>
-          <h2> Shop By Categories</h2>
-        </div>
-        <div className="slider">
-          <div className="wrapper">
-            <i id="left" className="fa-solid fa-angle-left"></i>
-            <div className="carousel" id="slider_img">
-              {[
-                'https://cms-contents.pharmeasy.in/carousel_item/1466c422fcd-Sugerfree.png?dim=1440x0',
-                'https://cms-contents.pharmeasy.in/carousel_item/3eb4eb6bfbe-App_Himalaya.jpg?dim=1440x0',
-                'https://cms-contents.pharmeasy.in/carousel_item/62668bab7a1-AccuChek-min.png?dim=1440x0',
-                'https://cms-contents.pharmeasy.in/carousel_item/d0f595be419-Savlonv2.png?dim=1440x0',
-                'https://cms-contents.pharmeasy.in/carousel_item/3c344faad99-App_Featured-J.jpg?dim=1440x0',
-                'https://cms-contents.pharmeasy.in/carousel_item/d3481485e73-App_stayfree.jpg?dim=1440x0',
-                'https://cms-contents.pharmeasy.in/carousel_item/f2d61678901-Ensure.png?dim=1440x0',
-                './images/slider1/s8.jpg',
-              ].map((src, index) => (
-                <img src={src} alt="img" draggable="false" key={index} />
-              ))}
-            </div>
-            <i id="right" className="fa-solid fa-angle-right"></i>
-          </div>
-        </div>
+        <div><h2><b>Shop By Categories</b></h2></div>
       </div>
+      {/* <div className="slider">
+        <div className="wrapper">
+          <FontAwesomeIcon id="left" icon={faAngleLeft} />
+          <div className="carousel" id="slider_img">
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/1466c422fcd-Sugerfree.png?dim=1440x0" alt="img" draggable="false" />
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/3eb4eb6bfbe-App_Himalaya.jpg?dim=1440x0" alt="img" draggable="false" />
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/62668bab7a1-AccuChek-min.png?dim=1440x0" alt="img" draggable="false" />
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/d0f595be419-Savlonv2.png?dim=1440x0" alt="img" draggable="false" />
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/3c344faad99-App_Featured-J.jpg?dim=1440x0" alt="img" draggable="false" />
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/d3481485e73-App_stayfree.jpg?dim=1440x0" alt="img" draggable="false" />
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/f2d61678901-Ensure.png?dim=1440x0" alt="img" draggable="false" />
+            
+          </div>
+          <FontAwesomeIcon id="right" icon={faAngleRight} />
+        </div>
+      </div> */}
 
-      <div id="banner">
-        <img width="90%" src="./images/offer/banner.jpg" alt="" />
+      {/* <Carousel/> */}
+      
+      <div className="banner-4">
+        <a href="#"><img src=' https://medvalue5.web.app/images/offer/banner.jpg'></img></a>
       </div>
 
       <div id="concern">
         <div className="con-1">
-          <h2>Shop by Concern</h2>
+          <h1><b>Shop by Concern</b></h1>
           <p>Products are handpicked by experts</p>
         </div>
         <div className="con-2">
-          {[
-            {
-              src: 'https://cms-contents.pharmeasy.in/homepage_top_categories_images/923a665cc6f-Skin_care.png?dim=128x0',
-              text: 'Skin Care',
-            },
-            {
-              src: 'https://cms-contents.pharmeasy.in/homepage_top_categories_images/18d2e2ee86b-Vitamins.png?dim=128x0',
-              text: 'Vitamins & Supplements',
-            },
-            {
-              src: 'https://cms-contents.pharmeasy.in/homepage_top_categories_images/0af9ac9f350-Diabetes.webp?dim=128x0',
-              text: 'Diabetes Care & Sugar',
-            },
-            {
-              src: 'https://cms-contents.pharmeasy.in/homepage_top_categories_images/24a0d2c733e-Heart.webp?dim=128x0',
-              text: 'Cardiac Care',
-            },
-            {
-              src: 'https://cms-contents.pharmeasy.in/homepage_top_categories_images/68369c9df98-Pregnancy.webp?dim=128x0',
-              text: 'Baby & Mom Care',
-            },
-            {
-              src: 'https://cms-contents.pharmeasy.in/homepage_top_categories_images/26bbd7a9e98-Lifestyle.webp?dim=128x0',
-              text: 'Lifestyle Disorders',
-            },
-            {
-              src: 'https://cms-contents.pharmeasy.in/homepage_top_categories_images/16ab65c0826-Covid.webp?dim=128x0',
-              text: 'Lifestyle Disorders',
-            },
-          ].map((item, index) => (
-            <div key={index}>
-              <img src={item.src} alt="" />
-              <h5>{item.text}</h5>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div id="article">
-        <h3>Health Articles</h3>
-        <div id="art-image">
-          {[
-            'https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125332/12-5.jpg?dim=1440x0',
-            'https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125500/9-3.jpg?dim=1440x0',
-            'https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125628/6-3.jpg?dim=1440x0',
-            'https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125758/19.jpg?dim=1440x0',
-            'https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125931/4-6.jpg?dim=1440x0',
-          ].map((src, index) => (
-            <a href="#" key={index}>
-              <div>
-                <img src={src} alt="" />
-                <p>
-                  Benefits of Bakasana ( Crane Pose) and How to Do it By Dr. Ankit Sankhe
-                </p>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
-
-      <div className="slider">
-        <div className="wrapper">
-          <i id="left" className="fa-solid fa-angle-left"></i>
-          <div className="carousel" id="slider_img">
-            {[
-              'https://cdn01.pharmeasy.in/dam/banner/banner/810889b6613-babycare-min.png?dim=1440x0',
-              'https://cdn01.pharmeasy.in/dam/banner/banner/5e9dc201ab9-DevicesClearanceStore.jpg?dim=1440x0',
-              'https://cdn01.pharmeasy.in/dam/banner/banner/ed873a24417-healthcare-min.png?dim=1440x0',
-              'https://cdn01.pharmeasy.in/dam/banner/banner/1d4f09c5b46-HHbrand.png?dim=1440x0',
-              'https://cdn01.pharmeasy.in/dam/banner/banner/668b87d949b-HH.jpg?dim=1440x0',
-              'https://cdn01.pharmeasy.in/dam/banner/banner/c02baead885-Mamaearth.png?dim=1440x0',
-              'https://cdn01.pharmeasy.in/dam/banner/banner/997d672b84e-healthfood.jpg?dim=1440x0',
-              'https://cdn01.pharmeasy.in/dam/banner/banner/3c85f11a202-health-library-min.png?dim=1440x0',
-              './images/slider1/s8.jpg',
-            ].map((src, index) => (
-              <img src={src} alt="img" draggable="false" key={index} />
-            ))}
+          <div>
+            <img src="https://cms-contents.pharmeasy.in/homepage_top_categories_images/923a665cc6f-Skin_care.png?dim=128x0" alt="" />
+            <h5>Skin Care</h5>
           </div>
-          <i id="right" className="fa-solid fa-angle-right"></i>
+          <div>
+            <img src="https://cms-contents.pharmeasy.in/homepage_top_categories_images/18d2e2ee86b-Vitamins.png?dim=128x0" alt="" />
+            <h5>Vitamins & Supplements</h5>
+          </div>
+          <div>
+            <img src="https://cms-contents.pharmeasy.in/homepage_top_categories_images/0af9ac9f350-Diabetes.webp?dim=128x0" alt="" />
+            <h5>Diabetes Care & Sugar</h5>
+          </div>
+          <div>
+            <img src="https://cms-contents.pharmeasy.in/homepage_top_categories_images/24a0d2c733e-Heart.webp?dim=128x0" alt="" />
+            <h5>Cardiac Care</h5>
+          </div>
+          <div>
+            <img src="https://cms-contents.pharmeasy.in/homepage_top_categories_images/68369c9df98-Pregnancy.webp?dim=128x0" alt="" />
+            <h5>Baby & Mom Care</h5>
+          </div>
+          <div>
+            <img src="https://cms-contents.pharmeasy.in/homepage_top_categories_images/26bbd7a9e98-Lifestyle.webp?dim=128x0" alt="" />
+            <h5>Lifestyle Disorders</h5>
+          </div>
+          <div>
+            <img src="https://cms-contents.pharmeasy.in/homepage_top_categories_images/16ab65c0826-Covid.webp?dim=128x0" alt="" />
+            <h5> LifeStyle Disorder</h5>
+          </div>
+          {/* <div>
+            <img src="https://cms-contents.pharmeasy.in/homepage_top_categories_images/59c8cc6677a-Weight_care.png?dim=128x0" alt="" />
+            <h5>Weight Loss</h5>
+          </div> */}
         </div>
       </div>
 
-      <div id="pop-brands">
-        <h2>Popular Brands</h2>
+      <div id="health">
+        <div className="health-1">
+          <h2>Health Articles</h2>
+          {/* <p>Get up-to-date information from our healthcare experts</p> */}
+        </div>
+        <div className="health-2">
+
+          <a href="#"><div><img src="https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125332/12-5.jpg?dim=1440x0" alt=""></img>
+            <p>Benefits of Bakasana ( Crane Pose) and How to Do it By Dr. Ankit Sankhe</p>
+          </div></a>
+          {/* 
+            <a href=""><div><img src="https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125628/6-3.jpg?dim=1440x0" alt=""></img>
+              <p>Benefits of Bakasana ( Crane Pose) and How to Do it By Dr. Ankit Sankhe</p>
+            </div></a> */}
+
+          <a href=""><div><img src="https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125500/9-3.jpg?dim=1440x0" alt=""></img>
+            <p>Benefits of Bakasana ( Crane Pose) and How to Do it By Dr. Ankit Sankhe</p>
+          </div></a>
+
+          <a href=""><div><img src="https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125758/19.jpg?dim=1440x0" alt=""></img>
+            <p>Benefits of Bakasana ( Crane Pose) and How to Do it By Dr. Ankit Sankhe</p></div></a>
+
+          <a href=""><div><img src="https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2022/10/06125931/4-6.jpg?dim=1440x0" alt=""></img>
+            <p>Benefits of Bakasana ( Crane Pose) and How to Do it By Dr. Ankit Sankhe</p>
+          </div></a>
+        </div>
       </div>
-      <div id="last">
-        {[...Array(10)].map((_, index) => (
-          <img src={`./images/footer/footer${index + 1}.png`} alt="" key={index} />
-        ))}
+
+      <div className="banner-2">
+        <a href="#"><img src='https://medvalue5.web.app/images/offer/b2.jpg'></img></a>
       </div>
+
+      <div className="banner-3">
+        <a href="#"><img src='https://medvalue5.web.app/images/offer/b3.jpg'></img></a>
+      </div>
+     
+
+      <div className="customer-comments">
+        <div className="con-1">
+          <h1><b>What Our Customers Have To Say</b></h1>
+        </div>
+        <div className="comments">
+          <div className="person">
+            <h5><b>Ramjee Rai</b></h5>
+            <p>April 16,2023</p>
+
+            <div className="comt">
+              <p><span><i class="fa-solid fa-quote-left"></i></span><br></br>The app is really wonderful. Being a Product Manager myself, I would say that the User experience (UI/UX) of the app is top notch (easy to use, simple and convenient). Coming to services and delivery, I would say Pharmeasy is doing a tremendous job even during this unprecedented pandemic situation.</p>
+            </div>
+          </div>
+
+          <div className="person">
+            <h5><b>Dipak Mourya</b></h5>
+            <p>August 15,2024</p>
+
+            <div className="comt">
+              <p><span><i class="fa-solid fa-quote-left"></i></span><br></br>Best service and app amongst all available. I have been using it for more than 3 years, and even during the pandemic, they have kept their standards high and are delivering the order within 24 hours. Keep up the good work.</p>
+            </div>
+          </div>
+
+          <div className="person">
+            <h5><b>Lokesh Nahar</b></h5>
+            <p>February 19,2023</p>
+
+            <div className="comt">
+              <p><span><i class="fa-solid fa-quote-left"></i></span><br></br>This app is a game changer for me. I am unable to go out always to buy medicinal products. Pharmeasy gives me the last liberty to shop essential healthcare products from home. The app is very user friendly and me being an elderly person do not find any difficulty in using it. They deliver well in time. Thanks😊</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <InfoSection/>
     </div>
   );
-}
+};
 
-export default Home;
+export default HomePage;
